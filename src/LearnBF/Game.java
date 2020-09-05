f976f63bcee5aaf08ea27f46827f842ffb064212
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-09-05 09:16:16 +0200

    Correct constructor created

--- src/LearnBF/Game.java
+++ src/LearnBF/Game.java
@@ -8,9 +8,16 @@ import java.util.Scanner;
 
 public class Game {
 
+    String randomMovie;
+    private List<String> fullList;
     File file = new File("files/movies.txt");
     Scanner fileScanner;
     List<String> listOfMovies = new ArrayList<>();
+
+    public Game(){
+        fullList=readFiles();
+        randomMovie=expressionDraw();
+    }
 
     {
         try {
@@ -20,26 +27,25 @@ public class Game {
         }
     }
 
-    public void readFiles() {
+    public List<String> readFiles() {
         while (fileScanner.hasNextLine()) {
             String line = fileScanner.nextLine();
             listOfMovies.add(line);
         }
+        return listOfMovies;
     }
 
     public String expressionDraw() {
-        int random = (int) (Math.random() * listOfMovies.size());
-        String randomMovie = listOfMovies.get(random);
-//        System.out.println(randomMovie);
+        int random = (int) (Math.random() * fullList.size());
+        randomMovie = fullList.get(random);
         return randomMovie;
     }
 
 
     public void changeToUnderscore() {
-        String s = expressionDraw();
+        String s = randomMovie;
         System.out.println(s);
         char[] sAsArray = s.toCharArray();
-//        System.out.println(s.toString() + "  " + sAsArray.length);
         char[] underscore = new char[sAsArray.length];
         for (int i = 0; i < sAsArray.length; i++) {
             if (sAsArray[i] == ' ') {
